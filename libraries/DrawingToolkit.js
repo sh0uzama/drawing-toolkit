0a6d9e866625d6568df3c989ba76d442aa9c3ba8
Paolo Lombardo <paolo.lombardo@gmail.com>
2017-08-07 12:14:52 +0200

    Add files via upload

---
+++ libraries/DrawingToolkit.js
@@ -0,0 +1,6 @@
+﻿var basePath = '/libraries';
+mxLoadResources = false;
+mxBasePath = basePath + '/mxGraph';
+window.RESOURCE_BASE = basePath + '/resources/grapheditor';
+window.STYLE_PATH = basePath + '/styles';
+window.STENCIL_PATH = basePath + '/stencils';
